14a5f5bf5a97cbe636ff8dbfbb82c2845dea120e
Jessica Bensch <jessica.bensch11@gmail.com>
2017-12-05 18:49:28 -0500

    write user_books migration

--- db/migrate/20171205234057_book_genres.rb
+++ db/migrate/20171205234057_book_genres.rb
@@ -1,5 +1,6 @@
 class BookGenres < ActiveRecord::Migration
   def change
+    create_table :book_genres do |t|
     t.integer :book_id
     t.integer :genre_id
   end
--- db/migrate/20171205234110_user_books.rb
+++ db/migrate/20171205234110_user_books.rb
@@ -1,4 +1,8 @@
 class UserBooks < ActiveRecord::Migration
   def change
+    create_table :user_books do |t|
+      t.integer :user_id
+      t.integer :book_id
+    end
   end
 end
