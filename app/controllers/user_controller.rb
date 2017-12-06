4601c61272adf1f067883cfe30657cc0def26c0c
Jessica Bensch <jessica.bensch11@gmail.com>
2017-12-06 11:56:56 -0500

    routes for login and signup

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,18 @@
+class UserController < ApplicationController
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+
+  end
+
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+  post '/signup' do
+    
+  end
+end
