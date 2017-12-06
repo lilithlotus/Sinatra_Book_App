7594e3b3c96c79464c2baeb3dc177a85357164e4
Jessica Bensch <jessica.bensch11@gmail.com>
2017-12-06 13:59:52 -0500

    add form to create a book

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,10 +1,11 @@
 class BookController < ApplicationController
 
   get '/books/new' do
+    @genres = Genre.all
     erb :'books/create_book'
   end
 
   post '/books/new' do
-    
+    @book = Book.create(params)
   end
 end
